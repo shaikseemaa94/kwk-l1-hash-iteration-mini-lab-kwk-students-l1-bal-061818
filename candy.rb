4fb2801e6ea1d41834487405d63b08a5b1a5644d
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-21 15:12:37 +0000

    automatically backed up by learn

---
+++ candy.rb
@@ -0,0 +1,12 @@
+Scholars = ["Avani", "Grace", "Siobhan", "Patch", "Cassidy", "Jesse", "Seema",
+"Shay", "Ume", "Irha", "Ava", "Lilly", "Hadja"]
+candy = [ "skittles", "Hershey", "Reese", "Lollipops","Almond joy", "Twix", "fun dip", ]
+
+fav_candy = {}
+
+each_candy = 6
+Scholars.each do |scandy|
+  fav_candy[scandy]= candy[each_candy]
+  each_candy += 1
+  if each candy 
+  
