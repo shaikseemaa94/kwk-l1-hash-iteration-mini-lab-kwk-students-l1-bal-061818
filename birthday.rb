6d725c71b59a76c7c88d8a135631f63bb17df5ba
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-21 14:23:02 +0000

    automatically backed up by learn

---
+++ birthday.rb
@@ -0,0 +1,10 @@
+new_hash={}
+bdays = {"Grace" => 1030, "Seema" => 614, "Lilly" 201, "Anu" => 525, 
+  "Ume" => 1026, "Jesse" => 621, "Siobhan" => 924, "Hadja" => 101, 
+  "Patch" => 601, "Irha" => 114}
+
+puts "Whose birthday are you trying to obtain?"
+input = gets.chomp
+puts "The birthday of #{input} is #{bdays[input]}"
+
+
