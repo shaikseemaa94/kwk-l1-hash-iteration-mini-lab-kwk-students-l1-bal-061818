e092207ab2a814e23a58fd3cbcbe03e41c68af4e
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-21 14:32:41 +0000

    automatically backed up by learn

--- hashexample.rb
+++ hashexample.rb
@@ -19,28 +19,10 @@ fav_celebrities.each do |name,celebrity |
 end
 
 new_hash={}
-bdays = {"Grace" => 1030, "Seema" => 614, "Lilly"201, "Anu" => 525, 
-  "Ume" => 1026, "Jesse" => 621, "Siobhan" => 924, "Hadja" => 101, 
+bdays = {"Grace" => 1030, "Seema" => 614, "Lilly"201, "Anu" => 525,
+  "Ume" => 1026, "Jesse" => 621, "Siobhan" => 924, "Hadja" => 101,
   "Patch" => 601, "Irha" => 114}
 
 puts "Whose birthday are you trying to obtain?"
 input = gets.chomp
 puts "The birthday of #{input} is #{bdays[input]}"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
