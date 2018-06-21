b3bb065316f9f8903fefe0b5fcaf071aa22a7d32
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-21 14:20:54 +0000

    automatically backed up by learn

---
+++ hashexample.rb
@@ -0,0 +1,39 @@
+fav_celebrities = {}
+fav_celebrities={
+    "shay" => "Anthony Bourdaine :(",
+    "Grace" => "Shawn Mendes",
+    "Lily" => "Orlando Bloom",
+    "Ava" => "Taylor Swift",
+    "Avani" => "Channing Tatum",
+    "Ume" => "Harry Styles",
+    "Hadja" => "Rihanna",
+    "Anu" => "The Queen",
+    "Seema" => "Allison Kim",
+    "Jesse" => "Big Time Rush",
+    "Cassidy" => "Hannah Montana",
+}
+fav_celebrities["Emma"]="Ed Sheeran" #Add something to the hash
+
+fav_celebrities.each do |name,celebrity |
+    puts "#{name}'s favorite celebrity is #{celebrity}"
+end
+
+Scholors_Birthdays = {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
