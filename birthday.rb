e5c989107f1fdaa14b26d3ae67a2e27993f6c3a1
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-21 14:24:35 +0000

    automatically backed up by learn

--- birthday.rb
+++ birthday.rb
@@ -1,6 +1,16 @@
-bdays = {"Grace" => 1030, "Seema" => 614, "Lilly" 201, "Anu" => 525,
-  "Ume" => 1026, "Jesse" => 621, "Siobhan" => 924, "Hadja" => 101,
-  "Patch" => 601, "Irha" => 114,}
+bdays = {
+  "Grace" => 1030,
+  "Seema" => 614,
+  "Lilly" 201,
+  "Anu" => 525,
+  "Ume" => 1026,
+  "Jesse" => 621,
+  "Siobhan" => 924,
+  "Hadja" => 101,
+  "Patch" => 601,
+  "Irha" => 114,
+
+}
 
 puts "Whose birthday are you trying to obtain?"
 input = gets.chomp
